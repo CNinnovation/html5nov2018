33b3eb44b3c090b2376790c09e3492aece60a16e
Christian Nagel <christian@christiannagel.com>
2018-11-14 08:53:44 +0100

    javascript samples

---
+++ JavaScriptSample/JavaScriptSample/scripts/Arrow1.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("button1").onclick = callnoarrow;
+    document.getElementById("button2").onclick = callarrow;
+});
+
+function callnoarrow() {
+    console.log(noarrow());
+}
+
+function callarrow() {
+    console.log(arrow());
+}
+
+function noarrow() {
+    var add = function (x, y) {
+        return x + y;
+    };
+    return add(38, 4);
+}
+
+function arrow() {
+    var add = (x, y) => x + y;
+
+    return add(11, 31);
+}
---
+++ JavaScriptSample/JavaScriptSample/scripts/Async1.js
@@ -0,0 +1,15 @@
+﻿function resolveAfter5Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 5000);
+    });
+}
+
+async function asyncCall() {
+    console.log('calling');
+    var result = await resolveAfter5Seconds();
+    console.log(result);
+}
+
+asyncCall();
