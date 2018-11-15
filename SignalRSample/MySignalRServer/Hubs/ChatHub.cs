35762458f53bec815979fbce7c9ba54f2014911f
Christian Nagel <christian@christiannagel.com>
2018-11-15 13:03:19 +0100

    signalR sample

---
+++ SignalRSample/MySignalRServer/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MySignalRServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void MyMessage(string user, string message)
+        {
+            Clients.All.SendAsync("MyBroadcast", user, message);
+        }
+    }
+}
---
+++ SignalRSample/MySignalRServer/wwwroot/chatclient.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const connection = new signalR.HubConnectionBuilder()
+        .configureLogging(signalR.LogLevel.Trace)
+        .withUrl("/mychat")
+        .build();
+
+    connection.on('MyBroadcast', (name, message) => {
+        console.log(message);
+        document.getElementById('output').innerHTML += `message from ${name}: ${message}<br />`;
+    });
+
+    connection.start().then(function () {
+        document.getElementById('sendbutton').addEventListener('click', function () {
+            let name = document.getElementById('myname').value;
+            let message = document.getElementById('myinput').value;
+
+            connection.invoke('myMessage', name, message);
+        });
+    });
+
+});
